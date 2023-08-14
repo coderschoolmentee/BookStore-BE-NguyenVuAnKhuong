ce503191cd2c3831d3bc8159fda737596e7e053b
Khuong <nvak1999001@gmail.com>
2023-08-14 20:35:44 +0700

    ad

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -33,21 +33,18 @@ bookController.getAllBooks = catchAsync(async (req, res, next) => {
   };
 
   if (search) {
-    const yearPattern = /\b\d{4}\b/; // Regular expression to match a 4-digit year
+    const yearPattern = /\b\d{4}\b/;
     const yearMatch = search.match(yearPattern);
 
     if (yearMatch) {
-      // Extracted year from search
       searchQuery.publicationDate = {
         $regex: new RegExp(`\\b${yearMatch[0]}\\b`),
       };
     } else {
-      // If no year pattern found, search other fields
       searchQuery.$or = [
         { name: { $regex: new RegExp(search, "i") } },
         { categories: { $regex: new RegExp(search, "i") } },
         { author: { $regex: new RegExp(search, "i") } },
-        { description: { $regex: new RegExp(search, "i") } },
         { publicationDate: { $regex: new RegExp(search, "i") } },
       ];
     }
